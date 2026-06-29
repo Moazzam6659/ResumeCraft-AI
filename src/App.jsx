9c9e192b92586ac661c105330756f06d4481b59e
Moazzam <moazzam.ali6659@gmail.com>
2026-06-29 16:40:03 +0530

    chore: complete project foundation setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
-    <main className='min-h-screen flex items-center justify-center bg-slate-950 text-white'>
-      <h1 className='text-5xl font-bold'>ResumeCraft AI 🚀</h1>
+    <main className="min-h-screen flex items-center justify-center bg-slate-950 text-white">
+      <h1 className="text-5xl font-bold">ResumeCraft AI 🚀</h1>
     </main>
   )
 }
